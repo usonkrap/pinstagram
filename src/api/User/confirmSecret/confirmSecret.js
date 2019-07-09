c9af6ee0d63762f4f0e1c7220d0baef3db04ec99
PARK Nosu <usonkrap@gmail.com>
2019-07-09 21:01:41 +0900

    follow/unfollw Resolver

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,7 +7,10 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (secret === user.loginSecret) {
-        // JWT
+        await prisma.updateUser({
+          where: { id: user.id },
+          data: { loginSecret: "" }
+        });
         return jwt.sign({ id: user.id }, process.env.JWT_SECRET);
       } else {
         throw Error("Wrong email/secret combination");
